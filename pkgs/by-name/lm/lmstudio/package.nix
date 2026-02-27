5327800fb6c27a3ce01e95c2882cc7309a5037bc
David Alexander Pfeiffer <20278364+davidmc971@users.noreply.github.com>
2026-02-27 09:29:03 +0100

    lmstudio: add aarch64-linux support

--- pkgs/by-name/lm/lmstudio/package.nix
+++ pkgs/by-name/lm/lmstudio/package.nix
@@ -11,6 +11,8 @@ let
   hash_aarch64-darwin = "sha256-mSszzDsoXv2D9Ky3K/P2Nn/mixq3HzGMonS1I4mz5+s=";
   version_x86_64-linux = "0.4.5-2";
   hash_x86_64-linux = "sha256-G+swwY9ff0Mqbdu8ZRbgm5KHrPhnOAaOYvH3dEVZ1+A=";
+  version_aarch64-linux = "0.4.5-2";
+  hash_aarch64-linux = "sha256-BeF9FNMde9RW2icdq07zkWQafge7ViWKvR+xupRIdjE=";
 
   meta = {
     description = "LM Studio is an easy to use desktop app for experimenting with local and open-source Large Language Models (LLMs)";
@@ -20,6 +22,7 @@ let
     maintainers = with lib.maintainers; [ crertel ];
     platforms = [
       "x86_64-linux"
+      "aarch64-linux"
       "aarch64-darwin"
     ];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
@@ -35,6 +38,16 @@ if stdenv.hostPlatform.isDarwin then
       args.url
         or "https://installers.lmstudio.ai/darwin/arm64/${version_aarch64-darwin}/LM-Studio-${version_aarch64-darwin}-arm64.dmg";
     hash = args.hash or hash_aarch64-darwin;
+  }
+else if stdenv.hostPlatform.isAarch64 then
+  callPackage ./linux.nix {
+    inherit pname meta;
+    passthru.updateScript = ./update.sh;
+    version = version_aarch64-linux;
+    url =
+      args.url
+        or "https://installers.lmstudio.ai/linux/arm64/${version_aarch64-linux}/LM-Studio-${version_aarch64-linux}-arm64.AppImage";
+    hash = args.hash or hash_aarch64-linux;
   }
 else
   callPackage ./linux.nix {
